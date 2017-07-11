9b9b33372b43bf7bc7a0bde3dd66278f51251c86
Shubhangi <sbisaria@tavisca.com>
2017-07-11 08:10:22 +0530

    Add queue implementation.

---
+++ Tavisca.Training2017.Basics.Queue/Tavisca.Training2017.Basics.QueueConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tavisca.Training2017.Basics.QueueImplementation;
+
+namespace Tavisca.Training2017.Basics.QueueConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of queue");
+            int size = Convert.ToInt32(Console.ReadLine());
+            if (size > 0)
+            {
+                Queue queue = new Queue(size);
+                int choice;
+                while (true)
+                {
+                    Console.WriteLine("Enter 0 to enqueue and 1 to dequeue and 2 to display");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    if (choice == 0)
+                    {
+                        if (queue.IsFull() == true)
+                        {
+                            Console.WriteLine("Queue is full");
+                        }
+                        else
+                        {
+							Console.WriteLine("Enter value to enqueue: ");
+                            queue.Enqueue(Convert.ToInt32(Console.ReadLine()));
+                        }
+                    }
+                    else if (choice == 1)
+                    {
+
+                        if (queue.IsEmpty() == true)
+                        {
+                            Console.WriteLine("Queue is empty");
+                        }
+                        else
+                        {
+
+                            int value = queue.Dequeue();
+                            Console.WriteLine("Dequeued value is : {0}", value);
+                        }
+                    }
+                    else if (choice == 2)
+                    {
+                        queue.Display();
+                    }
+                }
+            }
+            else
+                Console.WriteLine("Invalid size");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tavisca.Training2017.Basics.Queue/Tavisca.Training2017.Basics.QueueImplementation/Queue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tavisca.Training2017.Basics.QueueImplementation
+{
+    public class Queue
+    {
+        public int numberOfElemtsInQueue = 0;
+        int maxNumberOfElements = 0;
+        int[] numbers;
+        int rear = -1;
+        int front = 0;
+        
+        public Queue(int maxSize)
+        {
+            numbers = new int[maxSize];
+            maxNumberOfElements = maxSize;
+        }
+        public bool IsFull()
+        {
+            return numberOfElemtsInQueue == maxNumberOfElements;
+        }
+        public bool IsEmpty()
+        {
+            return numberOfElemtsInQueue == 0;
+        }
+        public void Enqueue(int numericVulaue)
+        {
+            rear += 1;
+            numbers[rear] = numericVulaue;
+            numberOfElemtsInQueue++;
+        }
+        public int Dequeue()
+        {
+            int dequeuedValue = numbers[front];
+            front += 1;
+            numberOfElemtsInQueue--;
+            IncreaseSizeBy1();
+            return dequeuedValue;
+        }
+
+        public void Display()
+        {
+            if (IsEmpty() == true)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Console.WriteLine("Values in queue are: ");
+                for (int i = front; i <= rear; i++)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+        }
+
+        private void IncreaseSizeBy1()
+        {
+            int[] tempArray = new int[numbers.Length + 1];
+            Array.Copy(numbers, tempArray, numbers.Length);
+            numbers = tempArray;
+        }
+    }
+}
