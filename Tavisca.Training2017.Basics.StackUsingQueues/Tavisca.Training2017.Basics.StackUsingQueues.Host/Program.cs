b402d2dfaa4a831dfc7c98a1e475701b40886587
Shubhangi <sbisaria@tavisca.com>
2017-07-11 08:12:50 +0530

    Add stack implementation using queues.

---
+++ Tavisca.Training2017.Basics.StackUsingQueues/Tavisca.Training2017.Basics.StackUsingQueues.Host/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tavisca.Training2017.Basics.StackUsingQueuesImpl;
+
+namespace Tavisca.Training2017.Basics.StackUsingQueues.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of stack");
+            int size = Convert.ToInt32(Console.ReadLine());
+            if (size > 0)
+            {
+                CustomStack stack = new CustomStack(size);
+                while (true)
+                {
+                    Console.WriteLine("Enter 0 to push, 1 to pop");
+                    int choice;
+                    choice = Convert.ToInt32(Console.ReadLine());
+                    if (choice == 0)
+                    {
+                        if (stack.IsFull() == true)
+                        {
+                            Console.WriteLine("Stack is full");
+                        }
+                        else
+                        {
+                            Console.Write("Enter the value you want to push: ");
+                            try
+                            {
+                                stack.Push(Convert.ToInt32(Console.ReadLine()));
+                            }
+                            catch (Exception exception)
+                            {
+                                Console.WriteLine("Invalid input");
+                            }
+                        }
+                    }
+                    else if (choice == 1)
+                    {
+                        if (stack.IsEmpty() == true)
+                        {
+                            Console.WriteLine("Stack is empty");
+                        }
+                        else
+                        {
+                            int returnedValue = stack.Pop();
+                            Console.WriteLine("Popped value is: {0}", returnedValue);
+                        }
+                    }
+                }
+            }
+            else
+                Console.WriteLine("Invalid size");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tavisca.Training2017.Basics.StackUsingQueues/Tavisca.Training2017.Basics.StackUsingQueuesImpl/CustomStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tavisca.Training2017.Basics.QueueImplementation;
+
+namespace Tavisca.Training2017.Basics.StackUsingQueuesImpl
+{
+    public class CustomStack
+    {
+        Queue queue1;
+        Queue queue2;
+        public CustomStack(int maxSize)
+        {
+            queue1 = new Queue(maxSize);
+            queue2 = new Queue(maxSize);
+        }
+
+        public bool IsEmpty()
+        {
+            return queue1.IsEmpty();
+        }
+
+        public bool IsFull()
+        {
+            return queue1.IsFull();
+        }
+
+        public void Push(int numericValue)
+        {
+            queue1.Enqueue(numericValue);
+        }
+
+        public int Pop()
+        {
+            if (queue1.numberOfElemtsInQueue == 1)
+                return queue1.Dequeue();
+            for (int index = 0; index < queue1.numberOfElemtsInQueue; index++)
+            {
+                int value = queue1.Dequeue();
+                queue2.Enqueue(value);
+            }
+            int poppedNumber = queue1.Dequeue();
+            while(queue2.numberOfElemtsInQueue>0)
+            {
+                int value2 = queue2.Dequeue();
+                queue1.Enqueue(value2);
+            }
+            return poppedNumber;
+        }
+    }
+}
