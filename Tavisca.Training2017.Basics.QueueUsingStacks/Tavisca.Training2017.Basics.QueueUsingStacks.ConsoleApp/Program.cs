f3aaeb589c46115f3fbff36987c01cc678c61e9c
Shubhangi <sbisaria@tavisca.com>
2017-07-11 08:11:36 +0530

    Add queue implementation using stacks.

---
+++ Tavisca.Training2017.Basics.QueueUsingStacks/Tavisca.Training2017.Basics.QueueUsingStacks.ConsoleApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tavisca.Training2017.Basics.QueueUsingStacksImpl;
+
+namespace Tavisca.Training2017.Basics.QueueUsingStacks.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of queue");
+            int size = Convert.ToInt32(Console.ReadLine());
+            if (size > 0)
+            {
+                CustomQueue queue = new CustomQueue(size);
+                int choice;
+                while (true)
+                {
+                    Console.WriteLine("Enter 0 to enqueue and 1 to dequeue and 2 to display");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    if (choice == 0)
+                    {
+                        if (queue.IsFull())
+                        {
+                            Console.WriteLine("Queue is full");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Enter a value");
+                            try
+                            {
+                                queue.Enqueue(Convert.ToInt32(Console.ReadLine()));
+                            }
+                            catch (Exception exception)
+                            {
+                                Console.WriteLine("Invalid input");
+                            }
+                        }
+                    }
+                    else if (choice == 1)
+                    {
+                        if (queue.IsEmpty())
+                        {
+                            Console.WriteLine("Queue is empty");
+                        }
+                        else
+                        {
+                            int value = queue.Dequeue();
+                            Console.WriteLine("Dequeued value is : {0}", value);
+                        }
+                    }
+                    else if (choice == 2)
+                    {
+                        if (queue.IsEmpty())
+                        {
+                            Console.WriteLine("Queue is empty");
+                        }
+                        else
+                        {
+                            queue.Display();
+                        }
+                    }
+                }
+            }
+            else
+                Console.WriteLine("Invalid size");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tavisca.Training2017.Basics.QueueUsingStacks/Tavisca.Training2017.Basics.QueueUsingStacksImpl/CustomQueue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tavisca.Training2017.Basics.StackImplementation;
+
+namespace Tavisca.Training2017.Basics.QueueUsingStacksImpl
+{
+    public class CustomQueue
+    {
+        Stack primaryStack;
+        Stack secondaryStack;
+
+        public CustomQueue(int maxSize)
+        {
+            primaryStack = new Stack(maxSize);
+            secondaryStack = new Stack(maxSize);
+        }
+        public bool IsFull()
+        {
+            return primaryStack.IsFull();
+        }
+        public bool IsEmpty()
+        {
+            return primaryStack.IsEmpty();
+        }
+
+        public void Enqueue(int numericValue)
+        {
+            primaryStack.Push(numericValue);
+        }
+
+        public int Dequeue()
+        {
+            
+            while (primaryStack.IsEmpty() == false)
+            {
+                int value = primaryStack.Pop();
+                secondaryStack.Push(value);
+            }
+            int poppedValue = secondaryStack.Pop();
+            while (secondaryStack.IsEmpty() == false)
+            {
+                int value = secondaryStack.Pop();
+                primaryStack.Push(value);
+            }
+            return poppedValue;
+        }
+
+        public void Display()
+        {
+            while (primaryStack.IsEmpty() == false)
+            {
+                int value = primaryStack.Pop();
+                secondaryStack.Push(value);
+            }
+            secondaryStack.Display();
+            while (secondaryStack.IsEmpty() == false)
+            {
+                int value = secondaryStack.Pop();
+                primaryStack.Push(value);
+            }
+        }
+    }
+}
